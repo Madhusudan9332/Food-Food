312dc3bb435b76ed4c7e5a2cb59fd91f70d22249
madhusudan9332 <madhusudangautam003@gmail.com>
2024-06-13 17:54:25 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import { ApiProvider } from "./context/ApiContext";
+import { ApiProvider } from "./context";
 import App from './App'
 import './index.css'
 
