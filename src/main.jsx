a90be805d4713488348ee3185c47e7e9f8fed17c
madhusudan9332 <madhusudangautam003@gmail.com>
2024-06-13 17:39:16 +0530

    minor change

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import { ApiProvider } from "./context";
+import { ApiProvider } from "./context/ApiContext";
 import App from './App'
 import './index.css'
 
